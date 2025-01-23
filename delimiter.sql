99c8ea854add3411eeaa91650746ac880ad86792
Aman Vishwakarma <143378283+its-Aman007@users.noreply.github.com>
2025-01-23 14:49:12 +0530

    Add files via upload

---
+++ delimiter.sql
@@ -0,0 +1,14 @@
+delimiter $$
+create function add_two_num(num1 int, num2 int)
+returns int
+deterministic
+begin
+declare num int;
+set num = num1 + num2;
+  return num;
+end$$
+delimiter ;
+select add_two_num(2,4);
+
+delimiter $$
+create function cal_age()
